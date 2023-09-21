875d9bdfb81fdb8d07870c54a85b0fa6ffcc209c
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2023-09-21 16:26:56 -0500

    added in cards for each genre of article

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Arts from '../containers/ArtsContainer';
 import Opinion from '../containers/OpinionsContainer';
 import Politics from '../containers/PoliticsContainer';
 import Sports from '../containers/SportsContainer';
 import Tech from '../containers/TechContainer';
+import axios from 'axios'
+
+const key = process.env.REACT_APP_API_KEY;
 
 function MainPage() {
+
+    const [arts, setArts] = React.useState(null);
+    const [tech, setTech] = React.useState(null);
+    const [opinion, setOpinion] = React.useState(null);
+    const [us, setUS] = React.useState(null);
+
+    useEffect(() => {
+        Promise.all([
+            axios.get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${key}`),
+            axios.get(`https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=${key}`),
+            axios.get(`https://api.nytimes.com/svc/topstories/v2/sundayreview.json?api-key=${key}`),
+            axios.get(`https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=${key}`),
+            axios.get(`https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=${key}`)
+        ]).then(data => {
+            setArts(data[0].data.results);
+            setTech(data[1].data.results);
+            setOpinion(data[2].data.results);
+            setUS(data[3].data.results);
+            console.log(data[4].data)
+        })
+    }, []);
+
+
     return (
         <div className='main-body'>
             <div className="nyt-main-header">
@@ -14,13 +40,12 @@ function MainPage() {
             </div>
             <div className="section-container-mainpage">
                 <div className='horizontal-sections'>
-                    <Arts />
-                    <Opinion />
-                    <Sports />
-                    <Tech />
+                    {arts && <Arts articles={arts} />}
+                    {opinion && <Opinion articles={opinion} />}
+                    {tech && <Tech articles={tech} />}
                 </div>
                 <div className='vertical-section'>
-                    <Politics /> 
+                    {us && <Politics articles={us} />}
                 </div>
             </div>
         </div>
